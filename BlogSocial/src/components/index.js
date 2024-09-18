a5e85483d410d02bd2214c08e2dafb515a4ceafe
Akansha Gupta <akshug1210@gmail.com>
2024-09-18 18:22:36 +0530

    AuthLayout

--- BlogSocial/src/components/index.js
+++ BlogSocial/src/components/index.js
@@ -3,11 +3,20 @@ import Footer from "./Footer/Footer";
 import Container from './container/Container'
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from './Button';
+import Input from './Input'
+import Select from './Select'
+import PostCard from './PostCard'
+
 
 export{
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Button,
+    Input,
+    PostCard,
+    Select
 }
