92d282147e0468a72ea2eff65aace200d88770a1
Akansha Gupta <akshug1210@gmail.com>
2024-09-27 15:46:44 +0530

    hi

--- BlogSocial/src/components/RTE.jsx
+++ BlogSocial/src/components/RTE.jsx
@@ -13,6 +13,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
   control={control}
   render={({field: {onChange}})=>(
     <Editor
+    apiKey='emdma236ym8118hw2trrr2kuducz5sfto7picusf37xgubjr'
     initialValue='default value'
     init={
       {
